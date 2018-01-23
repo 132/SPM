7d1180adcca64ca283c222889b2609b72a80f1c2
132 <bialan132@gmail.com>
2018-01-23 09:34:58 +0100

    basic things for the project

---
+++ SPM_Project/source/farmTask.h
@@ -0,0 +1,14 @@
+#pagram once
+
+#include<>
+
+func
+int power;
+int[] listParamerter;
+
+method:
+
+func(int p, int lp:)
+cal(int a, int b);
+
+
---
+++ SPM_Project/source/pipeLine.h
@@ -0,0 +1,14 @@
+#pagram once
+
+#include<>
+
+pipe
+int n_number;
+int[] listRandom;
+
+method:
+
+create_(int a, int b:)
+cal(int a, int b);
+
+
